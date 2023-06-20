fd181ea03cf2dd74375b0092647cc1e5f3ae3e0d
RAM-BHARATH <ramtherapid007@gmail.com>
2023-06-21 00:17:13 +0530

    Updated contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 export default function Contact() {
     return(
         <div className="contact-div" id="contacts">
-            <h2>Contact Me</h2>
+            <h2>Find Me Online</h2>
             <div className="contact-container">
                 <div className="contact">
                     <a href="mailto:rambharathvadivel@gmail.com">gmail.com<span className="arrow"><img src="images/arrow.svg" alt="" /></span></a>
@@ -10,10 +10,13 @@ export default function Contact() {
                     <a href="https://github.com/RAM-BHARATH">github.com<span className="arrow"><img src="images/arrow.svg" alt="" /></span></a>
                 </div>
                 <div className="contact">
-                    <a href="https://www.instagram.com/ram__v__">instagram.com<span className="arrow"><img src="images/arrow.svg" alt="" /></span></a>
+                    <a href="https://www.kaggle.com/rambharath30">kaggle.com<span className="arrow"><img src="images/arrow.svg" alt="" /></span></a>
                 </div>
                 <div className="contact">
                     <a href="https://www.linkedin.com/in/ram-bharath-v-1800bb1a6/">linkedin.com<span className="arrow"><img src="images/arrow.svg" alt="" /></span></a>
+                </div>
+                <div className="contact">
+                    <a href="https://www.instagram.com/ram__v__">instagram.com<span className="arrow"><img src="images/arrow.svg" alt="" /></span></a>
                 </div>
             </div>
         </div>
